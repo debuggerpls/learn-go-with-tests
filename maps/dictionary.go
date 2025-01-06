10c9c2e1db23217fd1425cb012968e1b2f841277
D. Puplauskas <ngivingup@gmail.com>
2025-01-06 21:59:15 +0100

    add dictionary add method and tests

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,32 @@
 package maps
 
-func Search(m map[string]string, key string) string {
-	return m[key]
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+const (
+	ErrKeyNotFound = DictionaryErr("key not found")
+	ErrKeyExists   = DictionaryErr("key already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(key string) (string, error) {
+	value, ok := d[key]
+	if !ok {
+		return "", ErrKeyNotFound
+	}
+
+	return value, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, ok := d[key]
+	if ok {
+		return ErrKeyExists
+	}
+	d[key] = value
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,61 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, err := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertNoError(t, err)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown workd", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrKeyNotFound
+
+		assertError(t, err, want)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		want := "this is just a test"
+		dictionary.Add("test", want)
+
+		got, err := dictionary.Search("test")
+		assertNoError(t, err)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		key := "test"
+		value := "existing test"
+		dictionary := Dictionary{key: value}
+		err := dictionary.Add(key, "some new test")
+		assertError(t, err, ErrKeyExists)
+		got, err := dictionary.Search(key)
+		assertNoError(t, err)
+		assertStrings(t, got, value)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Errorf("expected no error, got %q", got)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
