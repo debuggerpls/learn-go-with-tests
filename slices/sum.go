5a0f93ba553981b4fc8ae1d7e4015e62291046c6
Deividas Puplauskas <ngivingup@gmail.com>
2025-01-06 10:21:56 +0100

    add slices package

--- slices/sum.go
+++ slices/sum.go
@@ -7,3 +7,25 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, v := range numbersToSum {
+		if len(v) != 0 {
+			sums = append(sums, Sum(v[1:]))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+
+	return sums
+}
